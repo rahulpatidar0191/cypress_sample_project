c2fdc58233f0af78fb60063e9b0475cf58a3e066
Rahul <42219389+rahulpatidar0191@users.noreply.github.com>
2020-10-01 16:10:57 -0700

    add iframe example (#3)

--- cypress/support/elements.js
+++ cypress/support/elements.js
@@ -2,13 +2,10 @@
 class elements {
 
     constructor() {
-
-
         this.radio =  ".radioButton"
         this.autoCompleteDropdown = "#autocomplete"
         this.dropDownExample = "#dropdown-class-example"
-
-
+        
         this.checkBoxesExample = "input[type='checkbox']"
      
         this.newWindowExample = " button[id='openwindow']"
@@ -19,12 +16,6 @@ class elements {
         this.hideButton = "#hide-textbox"
         this.displayedText = "#displayed-text"
         this.showButton = "#show-textbox"
-
-      
-
-      
-        
-
     }
 }
 module.exports = elements
