da66a96a0a93655fd4d40b369ba92f0b169aef2c
Rahul Patidar <rahulpatidar0191@gmail.com>
2021-01-07 09:08:07 -0800

    update cypress

--- cypress/integration/happyPath.spec.js
+++ cypress/integration/happyPath.spec.js
@@ -1,20 +1,25 @@
-/// <reference types="Cypress" />
+
 var Elements = require("../support/elements");
 var data = require("../support/data");
 
 describe("Sample project ", () => {
   it("Radio buttons example", () => {
     var goTo = new Elements();
-    cy.get(goTo.radio).eq(0).click(); //clicking on the first radio element out of 3
 
-    //to click on all the radio buttons one by one
-    cy.get(goTo.radio).each((el, i, $list) => {
-      el.click();
-    });
+    cy.get('#username').type("jaiparu52@gmail.com")
+
+    cy.get('#password').type('23481761a')
+
+    cy.get('.sign-in > .solid-button').click()
+
+    
+
 
     //addd an assertion here to make sure the desired option is selected
   });
 
+/*
+  
   it("Dropdown Example", () => {
     var goTo = new Elements();
     cy.get(goTo.dropDownExample)
@@ -83,8 +88,9 @@ describe("Sample project ", () => {
       getBody().contains("Some hidden element").should("not.be.visible");
     });
   });
+  */
 });
 
 before(() => {
-  cy.visit(Cypress.env("baseUrl"));
+  cy.visit("https://www.marketwatch.com/watchlist");
 });
